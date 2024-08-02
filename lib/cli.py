4f744048ecb4c459088d468f8a7a23d953fa26b7
Zacchaeus Ndirangu Mwangi <ndirangu.zmwangi@gmail.com>
2024-08-02 20:19:58 +0300

    feat: Add menus

---
+++ lib/cli.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import click
+from helpers import add_task, view_tasks, edit_task, delete_task
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+@click.argument('to_do')
+@click.argument('priority')
+def add(to_do, priority):
+    try:
+        add_task(to_do, priority)
+        click.echo('Task added successfully')
+    except ValueError as e:
+        click.echo(f'Error: {e}')
+
+@click.command()
+def view():
+    view_tasks()
+
+@click.command()
+@click.argument('task_id', type=int)
+@click.option('--to_do', default=None)
+@click.option('--priority', default=None)
+def edit(task_id, to_do, priority):
+    try:
+        edit_task(task_id, to_do, description, priority)
+        click.echo('Task edited successfully')
+    except ValueError as e:
+        click.echo(f'Error: {e}')
+
+@click.command()
+@click.argument('task_id', type=int)
+def delete(task_id):
+    try:
+        delete_task(task_id)
+        click.echo('Task deleted successfully')
+    except ValueError as e:
+        click.echo(f'Error: {e}')
+
+cli.add_command(add)
+cli.add_command(view)
+cli.add_command(edit)
+cli.add_command(delete)
+
+if __name__ == '__main__':
+    cli()
+
